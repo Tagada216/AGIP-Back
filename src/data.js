baa0a79df7b5a638a44a5d47cf4ca9d0857ca1b1
A487365 <A487365@FR07201170L.dsk.eur.msd.world.socgen>
2021-01-26 09:18:15 +0100

    link bdd dev

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ const log = console.log
 const Sequelize = require("sequelize")
 const sequelize = new Sequelize({
 	dialect: "sqlite",
-	storage: "V:/ITIM/GSI/TDC/PROBLEMES/07-ToolBoxTDC/BDD/TDC_AGIPROS_BDD-Master.sdb",
+	storage: "V:/ITIM/GSI/TDC/PROBLEMES/07-ToolBoxTDC/BDD/TDC_AGIPROS_BDD-Dev.sdb",
 	define: {
 		timestamps: false
 	}
