d30dbd7d57aac0a0e56fb1146e7649968106a535
A487423 <A487423@FR07199639L.dsk.eur.msd.world.socgen>
2021-05-17 12:29:31 +0200

    add branch

--- src/data.js
+++ src/data.js
@@ -14,6 +14,7 @@ const jwt = require("jsonwebtoken")
 const config = require("./config")
 const tokenList = {}
 
+
 ////////////////////////////////////////
 // Initialisation de la base de données.
 ////////////////////////////////////////
